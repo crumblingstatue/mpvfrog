d8cde00da4f20afa126b5b6a2ae869e162435ad9
crumblingstatue <radiantstatue@gmail.com>
2022-05-17 18:51:59 +0200

    Separate AnsiTerm state out into TermState
    This solves the borrowing issues with AnsiTerm.
    Credit to... someone on discord

--- src/ansi_term.rs
+++ src/ansi_term.rs
@@ -1,11 +1,71 @@
 use crate::ansi_parser::{AnsiParser, TermCmd};
 
 pub struct AnsiTerm {
+    term_state: TermState,
+    ansi_parser: AnsiParser,
+}
+
+struct TermState {
     width: u16,
     height: usize,
     cells: Vec<u8>,
     cursor: Cursor,
-    ansi_parser: AnsiParser,
+}
+
+impl TermState {
+    fn new(width: u16) -> Self {
+        Self {
+            width,
+            height: 0,
+            cells: Vec::new(),
+            cursor: Cursor::default(),
+        }
+    }
+    pub fn contents_to_string(&self) -> String {
+        let mut s = String::new();
+        for y in 0..self.height {
+            s.push_str(std::str::from_utf8(self.line_slice(y)).unwrap());
+            s.push('\n');
+        }
+        s
+    }
+    pub fn line_slice(&self, y: usize) -> &[u8] {
+        let from = y * self.width as usize;
+        let to = from + self.width as usize;
+        &self.cells[from..to]
+    }
+    fn put_char(&mut self, ch: u8) {
+        self.extend_while_cursor_past();
+        self.cells[self.cursor.index(self.width)] = ch;
+        self.cursor.x += 1;
+        if self.cursor.x >= self.width {
+            self.cursor.x = 0;
+            self.cursor.y += 1;
+        }
+    }
+    fn extend(&mut self) {
+        self.cells
+            .extend(std::iter::repeat(b' ').take(self.width as usize));
+        self.height += 1;
+    }
+    fn extend_while_cursor_past(&mut self) {
+        while self.cursor.y >= self.height {
+            self.extend();
+        }
+    }
+    fn line_feed(&mut self) {
+        self.cursor.x = 0;
+        self.cursor.y += 1;
+    }
+    fn erase_from_cursor_to_eol(&mut self) {
+        for x in self.cursor.x..self.width {
+            let idx = self.cursor.y * self.width as usize + x as usize;
+            if idx >= self.cells.len() {
+                break;
+            }
+            self.cells[idx] = b' ';
+        }
+    }
 }
 
 #[derive(Default)]
@@ -23,80 +83,28 @@ impl Cursor {
 impl AnsiTerm {
     pub fn new(width: u16) -> Self {
         Self {
-            width,
-            height: 0,
-            cells: Vec::new(),
-            cursor: Cursor::default(),
+            term_state: TermState::new(width),
             ansi_parser: AnsiParser::default(),
         }
     }
-    pub fn contents_to_string(&self) -> String {
-        let mut s = String::new();
-        for y in 0..self.height {
-            s.push_str(std::str::from_utf8(self.line_slice(y)).unwrap());
-            s.push('\n');
-        }
-        s
-    }
-    pub fn line_slice(&self, y: usize) -> &[u8] {
-        let from = y * self.width as usize;
-        let to = from + self.width as usize;
-        &self.cells[from..to]
-    }
     pub fn feed(&mut self, data: &[u8]) {
         self.ansi_parser.advance(data, |cmd| match cmd {
-            TermCmd::PutChar(c) => Self::put_char(
-                &mut self.cells,
-                &self.width,
-                &mut self.height,
-                &mut self.cursor,
-                c,
-            ),
-            TermCmd::CarriageReturn => self.cursor.x = 0,
-            TermCmd::LineFeed => Self::line_feed(&mut self.cursor),
-            TermCmd::CursorUp(n) => self.cursor.y = self.cursor.y.saturating_sub(n as usize),
-            TermCmd::EraseFromCursorToEol => {
-                for x in self.cursor.x..self.width {
-                    let idx = self.cursor.y * self.width as usize + x as usize;
-                    if idx >= self.cells.len() {
-                        break;
-                    }
-                    self.cells[idx] = b' ';
-                }
+            TermCmd::PutChar(c) => self.term_state.put_char(c),
+            TermCmd::CarriageReturn => self.term_state.cursor.x = 0,
+            TermCmd::LineFeed => self.term_state.line_feed(),
+            TermCmd::CursorUp(n) => {
+                self.term_state.cursor.y = self.term_state.cursor.y.saturating_sub(n as usize)
             }
+            TermCmd::EraseFromCursorToEol => self.term_state.erase_from_cursor_to_eol(),
         });
     }
-    fn put_char(cells: &mut Vec<u8>, width: &u16, height: &mut usize, cursor: &mut Cursor, ch: u8) {
-        Self::extend_while_cursor_past(cells, cursor, width, height);
-        cells[cursor.index(*width)] = ch;
-        cursor.x += 1;
-        if cursor.x >= *width {
-            cursor.x = 0;
-            cursor.y += 1;
-        }
-    }
-    fn extend(cells: &mut Vec<u8>, width: &u16, height: &mut usize) {
-        cells.extend(std::iter::repeat(b' ').take(*width as usize));
-        *height += 1;
-    }
-    fn extend_while_cursor_past(
-        cells: &mut Vec<u8>,
-        cursor: &mut Cursor,
-        width: &u16,
-        height: &mut usize,
-    ) {
-        while cursor.y >= *height {
-            Self::extend(cells, width, height);
-        }
-    }
-    fn line_feed(cursor: &mut Cursor) {
-        cursor.x = 0;
-        cursor.y += 1;
-    }
     pub fn reset(&mut self) {
-        self.cursor = Cursor::default();
-        self.cells.clear();
-        self.height = 0;
+        self.term_state.cursor = Cursor::default();
+        self.term_state.cells.clear();
+        self.term_state.height = 0;
         self.ansi_parser = AnsiParser::default();
+    }
+    pub fn contents_to_string(&self) -> String {
+        self.term_state.contents_to_string()
     }
 }
