8a1e7ce7158a985d618052dd06bca456b41d5c7c
crumblingstatue <radiantstatue@gmail.com>
2022-06-20 00:28:11 +0200

    Type system fun

--- src/ipc.rs
+++ src/ipc.rs
@@ -20,48 +20,85 @@ pub struct Properties {
     pub time_pos: f64,
 }
 
-enum Command<'a> {
-    SetPaused(bool),
-    ObserveProperty(&'a str),
-    SetVolume(u8),
-    SetSpeed(f64),
-    Seek(f64),
-    SetVideo(bool),
+trait Command {
+    type R: Serialize = [serde_json::Value; 3];
+    fn json_values(&self) -> Self::R;
+    fn to_command_json(&self) -> CommandJson<Self::R> {
+        CommandJson {
+            command: self.json_values(),
+        }
+    }
 }
 
-impl<'a> Command<'a> {
-    fn into_command_json(self) -> CommandJson {
-        let vec = match self {
-            Command::SetPaused(paused) => {
-                vec!["set_property".into(), "pause".into(), paused.into()]
+trait SetProperty {
+    const NAME: &'static str;
+    type Value: Into<serde_json::Value>;
+    fn value(&self) -> Self::Value;
+}
+
+impl<T: SetProperty> Command for T {
+    type R = [serde_json::Value; 3];
+
+    fn json_values(&self) -> Self::R {
+        [
+            "set_property".into(),
+            Self::NAME.into(),
+            self.value().into(),
+        ]
+    }
+}
+
+macro_rules! set_property_impl {
+    ($t:ty, $name:literal, $v:ty) => {
+        impl SetProperty for $t {
+            const NAME: &'static str = $name;
+            type Value = $v;
+            fn value(&self) -> Self::Value {
+                self.0
             }
-            Command::ObserveProperty(which) => {
-                vec!["observe_property".into(), 1.into(), which.into()]
-            }
-            Command::SetVolume(vol) => {
-                vec!["set_property".into(), "volume".into(), vol.into()]
-            }
-            Command::SetSpeed(speed) => {
-                vec!["set_property".into(), "speed".into(), speed.into()]
-            }
-            Command::Seek(pos) => {
-                vec!["set_property".into(), "time-pos".into(), pos.into()]
-            }
-            Command::SetVideo(show) => {
-                vec![
-                    "set_property".into(),
-                    "vid".into(),
-                    if show { 1.into() } else { false.into() },
-                ]
-            }
-        };
-        CommandJson { command: vec }
+        }
+    };
+}
+
+struct SetPaused(bool);
+set_property_impl!(SetPaused, "pause", bool);
+
+struct ObserveProperty<'a>(&'a str);
+
+impl<'a> Command for ObserveProperty<'a> {
+    fn json_values(&self) -> Self::R {
+        ["observe_property".into(), 1.into(), self.0.into()]
+    }
+}
+
+struct SetVolume(u8);
+set_property_impl!(SetVolume, "volume", u8);
+
+struct SetSpeed(f64);
+set_property_impl!(SetSpeed, "speed", f64);
+
+struct Seek(f64);
+set_property_impl!(Seek, "time-pos", f64);
+
+struct SetVideo(bool);
+
+impl SetProperty for SetVideo {
+    const NAME: &'static str = "vid";
+
+    type Value = serde_json::Value;
+
+    fn value(&self) -> Self::Value {
+        if self.0 {
+            1.into()
+        } else {
+            false.into()
+        }
     }
 }
 
 #[derive(Serialize)]
-struct CommandJson {
-    command: Vec<serde_json::Value>,
+struct CommandJson<T: Serialize> {
+    command: T,
 }
 
 impl Bridge {
@@ -72,17 +109,17 @@ impl Bridge {
             ipc_stream,
             observed: Default::default(),
         };
-        this.write_command(Command::ObserveProperty("speed"));
-        this.write_command(Command::ObserveProperty("volume"));
-        this.write_command(Command::ObserveProperty("time-pos"));
-        this.write_command(Command::ObserveProperty("duration"));
+        this.write_command(ObserveProperty("speed"));
+        this.write_command(ObserveProperty("volume"));
+        this.write_command(ObserveProperty("time-pos"));
+        this.write_command(ObserveProperty("duration"));
         this
     }
     pub fn toggle_pause(&mut self) {
-        self.write_command(Command::SetPaused(!self.observed.paused));
+        self.write_command(SetPaused(!self.observed.paused));
     }
-    fn write_command(&mut self, command: Command) {
-        let command_json = command.into_command_json();
+    fn write_command<C: Command>(&mut self, command: C) {
+        let command_json = command.to_command_json();
         let mut serialized = serde_json::to_vec(&command_json).unwrap();
         // Commands need to be terminated with newline
         serialized.push(b'\n');
@@ -144,15 +181,15 @@ impl Bridge {
         }
     }
     pub fn set_volume(&mut self, vol: u8) {
-        self.write_command(Command::SetVolume(vol));
+        self.write_command(SetVolume(vol));
     }
     pub fn set_speed(&mut self, speed: f64) {
-        self.write_command(Command::SetSpeed(speed));
+        self.write_command(SetSpeed(speed));
     }
     pub fn seek(&mut self, pos: f64) {
-        self.write_command(Command::Seek(pos));
+        self.write_command(Seek(pos));
     }
     pub fn set_video(&mut self, show: bool) {
-        self.write_command(Command::SetVideo(show));
+        self.write_command(SetVideo(show));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(let_else)]
+#![feature(let_else, associated_type_defaults)]
 
 mod app;
 mod bool_ext;
