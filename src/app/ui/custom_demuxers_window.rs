34452c147a69a61c12307bc58359c32b259c5358
crumblingstatue <radiantstatue@gmail.com>
2022-07-09 14:43:57 +0200

    Put custom demuxers ui inside scroll area

--- src/app/ui/custom_demuxers_window.rs
+++ src/app/ui/custom_demuxers_window.rs
@@ -1,4 +1,4 @@
-use egui_sfml::egui::{Color32, ComboBox, Context, RichText, Ui, Window};
+use egui_sfml::egui::{Color32, ComboBox, Context, RichText, ScrollArea, Ui, Window};
 
 use crate::{
     app::Core,
@@ -42,97 +42,100 @@ impl CustomDemuxersWindow {
     }
     fn window_ui(&mut self, app: &mut Core, ui: &mut Ui) {
         let mut idx = 0;
-        app.cfg.custom_players.retain_mut(|custom_player| {
-            let mut retain = false;
-            ui.group(|ui| {
-                ComboBox::new(idx, "Predicate")
-                    .selected_text(PredicateKind::from(&custom_player.predicate).label())
-                    .show_ui(ui, |ui| {
-                        ui.selectable_value(
-                            &mut custom_player.predicate,
-                            Predicate::BeginsWith(String::new()),
-                            PredicateKind::BeginsWith.label(),
-                        );
-                        ui.selectable_value(
-                            &mut custom_player.predicate,
-                            Predicate::HasExt(String::new()),
-                            PredicateKind::HasExt.label(),
-                        );
-                    });
-                match &mut custom_player.predicate {
-                    Predicate::BeginsWith(frag) => ui.text_edit_singleline(frag),
-                    Predicate::HasExt(ext) => ui.text_edit_singleline(ext),
-                };
-                ui.label("Command");
-                match self.edit_target {
-                    Some(EditTarget {
-                        index,
-                        which: EditTargetWhich::Command,
-                    }) if idx == index => {
-                        if ui.text_edit_singleline(&mut self.edit_buffer).lost_focus() {
-                            match Command::from_str(&self.edit_buffer) {
-                                Ok(cmd) => {
-                                    custom_player.reader_cmd = cmd;
-                                    self.error_label.clear();
+        ScrollArea::vertical().max_height(300.0).show(ui, |ui| {
+            app.cfg.custom_players.retain_mut(|custom_player| {
+                let mut retain = false;
+
+                ui.group(|ui| {
+                    ComboBox::new(idx, "Predicate")
+                        .selected_text(PredicateKind::from(&custom_player.predicate).label())
+                        .show_ui(ui, |ui| {
+                            ui.selectable_value(
+                                &mut custom_player.predicate,
+                                Predicate::BeginsWith(String::new()),
+                                PredicateKind::BeginsWith.label(),
+                            );
+                            ui.selectable_value(
+                                &mut custom_player.predicate,
+                                Predicate::HasExt(String::new()),
+                                PredicateKind::HasExt.label(),
+                            );
+                        });
+                    match &mut custom_player.predicate {
+                        Predicate::BeginsWith(frag) => ui.text_edit_singleline(frag),
+                        Predicate::HasExt(ext) => ui.text_edit_singleline(ext),
+                    };
+                    ui.label("Command");
+                    match self.edit_target {
+                        Some(EditTarget {
+                            index,
+                            which: EditTargetWhich::Command,
+                        }) if idx == index => {
+                            if ui.text_edit_singleline(&mut self.edit_buffer).lost_focus() {
+                                match Command::from_str(&self.edit_buffer) {
+                                    Ok(cmd) => {
+                                        custom_player.reader_cmd = cmd;
+                                        self.error_label.clear();
+                                    }
+                                    Err(e) => self.error_label = e.to_string(),
                                 }
-                                Err(e) => self.error_label = e.to_string(),
+                                self.edit_buffer.clear();
+                                self.edit_target = None;
                             }
-                            self.edit_buffer.clear();
-                            self.edit_target = None;
+                        }
+                        _ => {
+                            if ui
+                                .text_edit_singleline(
+                                    &mut custom_player.reader_cmd.to_string().unwrap(),
+                                )
+                                .gained_focus()
+                            {
+                                self.edit_buffer = custom_player.reader_cmd.to_string().unwrap();
+                                self.edit_target = Some(EditTarget {
+                                    index: idx,
+                                    which: EditTargetWhich::Command,
+                                });
+                            }
+                        }
+                    };
+                    if !self.error_label.is_empty() {
+                        ui.label(RichText::new(&self.error_label).color(Color32::RED));
+                    }
+                    ui.label("Example: my-cmd --input {}");
+                    ui.label("extra mpv args");
+                    match self.edit_target {
+                        Some(EditTarget {
+                            index,
+                            which: EditTargetWhich::MpvArgs,
+                        }) if idx == index => {
+                            if ui.text_edit_singleline(&mut self.edit_buffer).lost_focus() {
+                                custom_player.extra_mpv_args = self
+                                    .edit_buffer
+                                    .split_whitespace()
+                                    .map(|s| s.to_string())
+                                    .collect();
+                                self.edit_buffer.clear();
+                                self.edit_target = None;
+                            }
+                        }
+                        _ => {
+                            if ui
+                                .text_edit_singleline(&mut custom_player.extra_mpv_args.join(" "))
+                                .gained_focus()
+                            {
+                                self.edit_buffer = custom_player.extra_mpv_args.join(" ");
+                                self.edit_target = Some(EditTarget {
+                                    index: idx,
+                                    which: EditTargetWhich::MpvArgs,
+                                });
+                            }
                         }
                     }
-                    _ => {
-                        if ui
-                            .text_edit_singleline(
-                                &mut custom_player.reader_cmd.to_string().unwrap(),
-                            )
-                            .gained_focus()
-                        {
-                            self.edit_buffer = custom_player.reader_cmd.to_string().unwrap();
-                            self.edit_target = Some(EditTarget {
-                                index: idx,
-                                which: EditTargetWhich::Command,
-                            });
-                        }
-                    }
-                };
-                if !self.error_label.is_empty() {
-                    ui.label(RichText::new(&self.error_label).color(Color32::RED));
-                }
-                ui.label("Example: my-cmd --input {}");
-                ui.label("extra mpv args");
-                match self.edit_target {
-                    Some(EditTarget {
-                        index,
-                        which: EditTargetWhich::MpvArgs,
-                    }) if idx == index => {
-                        if ui.text_edit_singleline(&mut self.edit_buffer).lost_focus() {
-                            custom_player.extra_mpv_args = self
-                                .edit_buffer
-                                .split_whitespace()
-                                .map(|s| s.to_string())
-                                .collect();
-                            self.edit_buffer.clear();
-                            self.edit_target = None;
-                        }
-                    }
-                    _ => {
-                        if ui
-                            .text_edit_singleline(&mut custom_player.extra_mpv_args.join(" "))
-                            .gained_focus()
-                        {
-                            self.edit_buffer = custom_player.extra_mpv_args.join(" ");
-                            self.edit_target = Some(EditTarget {
-                                index: idx,
-                                which: EditTargetWhich::MpvArgs,
-                            });
-                        }
-                    }
-                }
-                retain = !ui.button("Delete demuxer").clicked();
+                    retain = !ui.button("Delete demuxer").clicked();
+                });
+                idx += 1;
+                retain
             });
-            idx += 1;
-            retain
         });
         if ui.button("add new demuxer").clicked() {
             app.cfg.custom_players.push(CustomPlayerEntry::default());
