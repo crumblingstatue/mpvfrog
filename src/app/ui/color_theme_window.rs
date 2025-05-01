d0f90d6c0f44b7b1c9d97ed0fdb0083dcba1fd94
crumblingstatue <radiantstatue@gmail.com>
2025-05-01 12:01:41 +0200

    Make resetting default color theme work correctly
    The context popup didn't update correctly when the default theme was
    reset (colorix removed).

--- src/app/ui/color_theme_window.rs
+++ src/app/ui/color_theme_window.rs
@@ -14,17 +14,22 @@ impl ColorThemeWindow {
         &mut self,
         core: &mut Core,
         ctx: &egui::Context,
-        colorix: &mut Option<Colorix>,
+        opt_colorix: &mut Option<Colorix>,
     ) {
         egui::Window::new("Color config")
             .open(&mut self.open)
             .show(ctx, |ui| {
-                let colorix = colorix.as_mut().unwrap();
+                let Some(colorix) = opt_colorix else {
+                    if ui.button("Enable custom theme").clicked() {
+                        *opt_colorix = Some(Colorix::global(ctx, egui_colors::utils::EGUI_THEME));
+                    }
+                    return;
+                };
+                let mut reset_default = false;
                 ui.horizontal(|ui| {
                     colorix.themes_dropdown(ui, None, false);
                     if ui.button("Reset default").clicked() {
-                        core.cfg.theme = None;
-                        ctx.set_visuals(egui::Visuals::dark());
+                        reset_default = true;
                     }
                     if ui.button("Random").clicked() {
                         let mut rng = rand::rng();
@@ -35,8 +40,17 @@ impl ColorThemeWindow {
                 });
                 ui.separator();
                 colorix.ui_combo_12(ui, true);
-
-                core.cfg.theme = Some(colorix.theme().map(|theme| theme.rgb()));
+                if reset_default {
+                    core.cfg.theme = None;
+                    ctx.set_visuals(egui::Visuals::dark());
+                    *opt_colorix = None;
+                    return;
+                }
+                // If we have a theme, update it from colorix
+                // (so when the user edits a color, it updates)
+                if let Some(theme) = &mut core.cfg.theme {
+                    *theme = colorix.theme().map(|theme| theme.rgb())
+                }
             });
     }
 }
